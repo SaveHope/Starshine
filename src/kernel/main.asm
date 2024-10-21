778cc39fa68342f1a01bc6a48d2f3c3c8ed32733
zeflite <crystal930endera@gmail.com>
2024-10-21 21:11:34 +0300

    Setup Memory Paging, and Long Mode.
    But something goes wrong, and PC enters to boot-loop

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -50,7 +50,18 @@ section .multiboot
 ;
 
 section .bss
-align 4
+;   Memory Page Tables
+align 4096
+mpt4:
+    resb 4096
+mpt3:
+    resb 4096
+mpt2:
+    resb 4096
+mpt1:
+    resb 4096
+;   Stack
+align 16
 stack_bottom:
     resb 16384 ; 16 KB
 stack_top:
@@ -62,14 +73,33 @@ stack_top:
 ;
 
 section .rodata
+;   Global Descriptor Table
+gdt64:
+    ;   Null-descriptor
+    dq 0
+.code: equ $ - gdt64
+    ;   Read     | Executable | Descriptor | Present   | x64
+    dq (1 << 41) | (1 << 43)  | (1 << 44)  | (1 << 47) | (1 << 53)
+.data: equ $ - gdt64
+    ;   Read     | Descriptor | Present
+    dq (1 << 41) | (1 << 44)  | (1 << 47)
+.pointer:
+    ;   Table size
+    dw $ - gdt64 - 1
+    ;   Table begin address
+    dq gdt64
+
+;   Output messages
 msg_empty:     db VGA_TEXT,' ', 0
 msg_done:      db VGA_DONE,' DONE ',0
 msg_failed:    db VGA_FAIL,' FAILED ',0
 msg_halted:    db VGA_TEXT,'Halt loop entering',0
 msg_bootbegin: db VGA_TEXT,'Booting ',VGA_LIGHT,'Starshine ',VGA_TEXT, 'OS',0
 msg_bootfault: db VGA_TEXT,'Boot fault.',0
-msg_cpuid:     db VGA_TEXT,'[1/2] CPUID check... ',0
-msg_longmode:  db VGA_TEXT,'[2/2] Long-mode check... ',0
+msg_cpuid:     db VGA_TEXT,'CPUID check... ',0
+msg_longmode:  db VGA_TEXT,'Long-mode check... ',0
+msg_setup_memorypages: db VGA_TEXT,'Setting up Memory Pages',0
+msg_setup_longmode:    db VGA_TEXT,'Setting up Long Mode',0
 
 
 
@@ -87,19 +117,28 @@ bits 32
 _start:
     ;   Setup stack
     mov esp, stack_top
-
     ;   Boot begin message
     WRITELINE msg_bootbegin
     WRITELINE msg_empty
-
-    ;   CPUID check
+    ;   Check
     call check_cpuid
-
-    ;   Long mode
     call check_longmode
+    ;   Setup
+    call setup_memorypages
+    call setup_longmode
+    ;   Jump to x64 code
+    ;jmp gdt64.code:x64start
 
-    ;   Enter into halt loop
-    call halt
+
+
+;   FUNCTION `halt`
+
+halt:
+    WRITELINE msg_halted
+    cli
+.loop:
+    hlt
+    jmp .loop
 
 
 
@@ -107,12 +146,11 @@ _start:
 
 check_cpuid:
     WRITE msg_cpuid
-
     ;   Pop EFLAGS to EAX (for checking) and EBX (for restoring)
     pushfd
     pop eax
     mov ebx, eax
-    ;   Enabling ID flag, and push EAX to EFLAGS
+    ;   Switch ID flag, and push EAX to EFLAGS
     xor eax, 1 << 21
     push eax
     popfd
@@ -151,18 +189,66 @@ check_longmode:
 
 
 
+;   FUNCTION `setup_memorypages`
+
+setup_memorypages:
+    ;   Point mpt3 to mpt4
+    mov eax, mpt3
+    or eax, 11b ; Present | Writeable
+    mov dword [mpt4], eax
+    ;   Point mpt2 to mpt3
+    mov eax, mpt2
+    or eax, 11b ; Present | Writeable
+    mov dword [mpt3], eax
+    ;   Point mpt1 to mpt2
+    mov eax, mpt1
+    or eax, 11b ; Present | Writeable
+    mov dword [mpt2], eax
+    ;   Mapping mpt1
+    mov ecx, 0
+.loop:
+    mov eax, 0x400 ; 2KB each page
+    mul ecx
+    or eax, 11b ; Present | Writeable
+    mov [mpt1 + ecx * 8], eax
+    ;   increment
+    inc ecx
+    cmp ecx, 512
+    jne .loop
+    ;   Set memory page address
+    mov eax, mpt4
+    mov cr3, eax
+    WRITELINE msg_setup_memorypages
+    ret
+
+;   FUNCTION `setup_longmode`
+
+setup_longmode:
+    ;   Enable Physical Address Extension
+    mov edx, cr4
+    or edx, 1 << 5
+    mov cr4, edx
+    ;   Enable Long Mode
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+    ;   Enable Memory Pages and protect read-only pages
+    mov eax, cr0
+    or eax, (1 << 16) | (1 << 31)
+    mov cr0, eax
+    ;   Enable GDT
+    lgdt [gdt64.pointer]
+    ;   Setup segment registers
+    mov ax, gdt64.data
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    WRITELINE msg_setup_longmode
+    ret
+
 ;   FUNCTION `failed`
 
 failed:
     WRITELINE msg_bootfault
     jmp halt
-
-
-
-;   FUNCTION `halt`
-
-halt:
-    WRITELINE msg_halted
-.loop:
-    hlt
-    jmp .loop
